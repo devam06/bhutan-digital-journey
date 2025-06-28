7e608b1b544ace5a2e98fe8e7bc33997b213f3ef
Devam Dubey <studentdevam2004@gmail.com>
2025-06-28 15:44:15 +0530

    Added firebase config

---
+++ firebase_config.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA0UH9dMXcmJEdf8SfGLkGw42QSe5BtTW8",
+  authDomain: "e-butan.firebaseapp.com",
+  projectId: "e-butan",
+  storageBucket: "e-butan.firebasestorage.app",
+  messagingSenderId: "278915916735",
+  appId: "1:278915916735:web:dae555bee556af9a91b70a"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
